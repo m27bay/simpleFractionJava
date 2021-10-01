e4653e92aea3ef1ca944536af4bda51246f42dbd
m27bay <mickael.ledenmat@gmail.com>
2021-10-01 21:28:34 +0200

    test Collection

--- exo4_7/partie_1/Collection.java
+++ exo4_7/partie_1/Collection.java
@@ -1,11 +1,50 @@
 package exo4_7.partie_1;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.print.Doc;
 
 public class Collection {
 	private List<Document> listeDocument;
+	private static final int NBR_MAX_DOC = 50;
 
-	Collection() {}
+	Collection() {
+		listeDocument = new ArrayList<Document>(NBR_MAX_DOC);
+	}
 
-	void ajout
+	void ajout(Document d) {
+		if(d == null) {
+			System.out.println("Erreur ajout : document vide.");
+		}
+		else {
+			listeDocument.add(d);
+		}
+	}
+	
+	void suppression(Document d) {
+		if(d == null) {
+			System.out.println("Erreur ajout : document vide.");
+		}
+		else if(listeDocument.isEmpty()) {
+			System.out.println("Erreur suppression : liste vide.");
+		}
+		else {
+			listeDocument.add(d);
+		}
+	}
+
+	@Override
+	public String toString() {
+		if(listeDocument.isEmpty()) {
+			return "Collection vide";
+		}
+		else {
+			String chaine = "";
+			for(Document d : listeDocument) {
+				chaine += d.toString() + "\n";
+			}
+			return chaine;
+		}
+	}
 }
--- exo4_7/partie_1/Main.java
+++ exo4_7/partie_1/Main.java
@@ -8,11 +8,16 @@ public class Main {
 		printf("\n");
 		Document cd = new CD("Off the Wall", "Michael Jackson", 1);
 		Document dvd = new DVD("Star Wars épisode 3 : La revange de Sith", "George Lucas", 2005);
-		
-		printf("\n");
 		printf(cd.toString());
 		printf(dvd.toString());
-
 		
+		printf("\n");
+		Collection c = new Collection();
+		printf(c.toString());
+		
+		printf("\n");
+		c.ajout(cd);
+		c.ajout(dvd);
+		printf(c.toString());
 	}
 }
