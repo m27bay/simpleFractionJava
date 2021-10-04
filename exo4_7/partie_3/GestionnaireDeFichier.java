d0c183c5dc8132fa51d87483d87943b4f6940763
m27bay <mickael.ledenmat@gmail.com>
2021-10-04 10:22:58 +0200

    ajout methode 'ajout' pour un dossier precis

--- exo4_7/partie_3/GestionnaireDeFichier.java
+++ exo4_7/partie_3/GestionnaireDeFichier.java
@@ -6,6 +6,7 @@ public class GestionnaireDeFichier {
 
 	GestionnaireDeFichier() {
 		racine = new Dossier("DATA");
+		racine.setPere(null);
 		courant = racine;
 	}
 
@@ -26,6 +27,40 @@ public class GestionnaireDeFichier {
 	
 	void ajout(Element e) {
 		courant.ajoutFils(e);
+	}
+
+	void ajout(Dossier d, Element e) {
+		if(d == courant) {
+			ajout(e);
+		}
+		else {
+			ajout(racine, d, e);
+		}
+	}
+
+	private void ajout(Dossier courant, Dossier objectif, Element e) {
+		if(courant == objectif) {
+			courant.ajoutFils(e);
+		}
+		else {
+			for(Element cible : courant.getfils()) {
+				if(cible instanceof Dossier) {
+					ajout((Dossier)cible, objectif, e);
+				}
+			}
+			
+		}
+	}
+
+	public String pwd() {
+		Dossier tmp = courant;
+		String pwdChaine = "";
+		pwdChaine += racine.getNom();
+		while(tmp != null && tmp != racine) {
+			pwdChaine += ("\\" + tmp.getNom());
+			tmp = tmp.getPere();			
+		}
+		return pwdChaine;
 	}
 
 	public Dossier getRacine() {
